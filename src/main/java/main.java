a263d274ca126e7d7e7b7f4c333f286ed9199ce2
liqinmin <468845441@qq.com>
2024-07-31 23:09:33 +0800

    修改1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
