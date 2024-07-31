af2a5476d7ba82b9519ba97f9341764e17a9b84c
liqinmin <468845441@qq.com>
2024-07-31 23:08:59 +0800

    修改1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("第一次修改");
+    }
 }
